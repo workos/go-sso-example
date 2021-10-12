a8b45892bd024e1b325d2af43fb7fe9efe269aae
Adam Wolfman <adamwolfman@Adams-MBP-3.domain>
2021-10-12 17:40:35 -0600

    Update UI with templates and add post login page to UX

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"flag"
+	"html/template"
 	"log"
 	"net/http"
 
@@ -14,34 +15,40 @@ func main() {
 	var conf struct {
 		Addr        string
 		APIKey      string
-		ClientID   string
+		ClientID    string
 		RedirectURI string
 		Domain      string
+		Provider    string
+	}
+
+	type Profile struct {
+		ProfileData string
 	}
 
 	flag.StringVar(&conf.Addr, "addr", ":3042", "The server addr.")
-	flag.StringVar(&conf.APIKey, "api-key", "", "The WorkOS API key.")
-	flag.StringVar(&conf.ClientID, "client-id", "", "The WorkOS project id.")
-	flag.StringVar(&conf.RedirectURI, "redirect-uri", "", "The redirect uri.")
-	flag.StringVar(&conf.Domain, "domain", "", "The domain used to register a WorkOS SSO connection.")
+	flag.StringVar(&conf.APIKey, "api-key", "sk_test_a2V5XzAxRkExMkM3TTNSTldFNUNKSEFNUUVZQ1pTLDJtb3drUExOTk9vT3dDc1NDRTZnRUVVQ28", "The WorkOS API key.")
+	flag.StringVar(&conf.ClientID, "client-id", "client_01FA12C7QV793K318T2G1V3E7X", "The WorkOS project id.")
+	flag.StringVar(&conf.RedirectURI, "redirect-uri", "http://localhost:3042/callback", "The redirect uri.")
+	flag.StringVar(&conf.Domain, "domain", "workos.com", "The domain used to register a WorkOS SSO connection.")
+	flag.StringVar(&conf.Provider, "provider", "MicrosoftOAuth", "The OAuth provider used for the SSO connection.")
 	flag.Parse()
 
 	log.Printf("launching sso demo with configuration: %+v", conf)
 
 	http.Handle("/", http.FileServer(http.Dir("./static")))
-  
-	
+
 	// Configure the WorkOS SSO SDK:
 	sso.Configure(conf.APIKey, conf.ClientID)
 
 	// Handle login
 	http.Handle("/login", sso.Login(sso.GetAuthorizationURLOptions{
 		//Instead of domain, you can now use connection ID to associate a user to the appropriate connection.
-		Domain: conf.Domain,
+		Provider:    "MicrosoftOAuth",
 		RedirectURI: conf.RedirectURI,
 	}))
 
 	// Handle login redirect:
+	tmpl := template.Must(template.ParseFiles("./static/logged_in.html"))
 	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("callback is called with %s", r.URL)
 
@@ -59,20 +66,24 @@ func main() {
 
 		// Display user profile:
 		b, err := json.MarshalIndent(profile, "", "    ")
+		print(b)
 		if err != nil {
 			log.Printf("encoding profile failed: %s", err)
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(err.Error()))
 		}
-		w.Write(b)
 
-		log.Printf("user is logged with profile: %s", b)
+		// Convert the profile to a string
+		stringB := string(b)
+		data := Profile{
+			ProfileData: stringB,
+		}
+
+		// Render the template
+		tmpl.Execute(w, data)
 	})
 
 	if err := http.ListenAndServe(conf.Addr, nil); err != nil {
 		log.Panic(err)
 	}
-
-	
 }
-
